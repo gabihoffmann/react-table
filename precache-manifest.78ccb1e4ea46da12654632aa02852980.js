53a890de85f0bba3d24c7e53a164d11e1d79bd14
gabihoffmann <ah.gabriella@gmail.com>
2020-05-06 18:52:22 -0300

    Updates

---
+++ precache-manifest.78ccb1e4ea46da12654632aa02852980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fef312f172187af7ecc56d778aabfab",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "b91bb5053de264093e29",
+    "url": "/react-table/static/css/main.e7d47112.chunk.css"
+  },
+  {
+    "revision": "0ddb8da5725b9f5385a5",
+    "url": "/react-table/static/js/2.9f7c4ac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-table/static/js/2.9f7c4ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91bb5053de264093e29",
+    "url": "/react-table/static/js/main.b0714aff.chunk.js"
+  },
+  {
+    "revision": "5b2b558e738dd66ffaa4",
+    "url": "/react-table/static/js/runtime-main.b0233b5d.js"
+  }
+]);
